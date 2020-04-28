55b17b64311f87cebf3fc9c8ba69fe467f4cad16
gomamon <leeye9779@gmail.com>
2020-04-29 03:30:19 +0900

    comment

--- hw1/clock.c
+++ hw1/clock.c
@@ -3,17 +3,23 @@
 
 
 void mode_clock(int sw, int inflag, struct clock_data *clock_info, struct outbuf* buf_out){
+    
+    /* variable for system time */
     struct tm *tm;
-    time_t raw_t = time(NULL); //get raw time
+    time_t raw_t = time(NULL);
     tm = localtime(&raw_t);
 
-
+    /* When key or switch pushed */
     if(inflag !=0){
         switch(sw){
-            case KEY_VOL:
+            case KEY_VOL:   
+                /*Initailize devices' output and clock mode*/
+                memset(buf_out->dot_matrix,0x00,MAX_DOT_MATRIX);
+                memset(buf_out->text_lcd, 0, MAX_TEXT_LCD);
                 clock_info->clock_mode = CLOCK_SHOW;
                 break;
-            case SW1:  // chage board time(led 3/4) or save;
+            case SW1:  
+                /*changes clock mode*/
                 switch (clock_info->clock_mode) {
                     case CLOCK_SHOW:
                         clock_info->clock_mode=CLOCK_MODI;
@@ -27,18 +33,21 @@ void mode_clock(int sw, int inflag, struct clock_data *clock_info, struct outbuf
                         break;
                 }
                 break;
-            case SW2: // reset to board time;
+            case SW2: 
+                /* reset to board time */
                 if(clock_info->clock_mode==CLOCK_MODI){
                     clock_info->unsaved_h = tm->tm_hour;
                     clock_info->unsaved_m = tm->tm_min ;
                 }
                 break;
-            case SW3: // hour++;
+            case SW3: 
+                /* In CLOCK_MODI mode, hour++ */
                 if(clock_info->clock_mode==CLOCK_MODI){
                     clock_info->unsaved_h = (clock_info->unsaved_h + 1)%24;
                 }
                 break;
-            case SW4: // minute ++;
+            case SW4:
+                /* In CLOCK_MODI mode, min++ */
                 if(clock_info->clock_mode==CLOCK_MODI){
                     clock_info->unsaved_m = (clock_info->unsaved_m + 1);
                     if(clock_info->unsaved_m){
@@ -50,28 +59,28 @@ void mode_clock(int sw, int inflag, struct clock_data *clock_info, struct outbuf
         }
     }
 
-     switch(clock_info->clock_mode){
+    /* Set devices' output(fnd, led) buffer to formatted data */
+    switch(clock_info->clock_mode){
         case CLOCK_SHOW:
+            /* set current_time(tm_hour, tm_min)+saved_time_diffrence(clock_h, clock_m) to fnd output in CLOCK_SHOW mode*/
             sprintf(buf_out->fnd,"%02d%02d",((tm->tm_hour+clock_info->clock_h)+(tm->tm_min+clock_info->clock_m)/60)%24,(tm->tm_min+clock_info->clock_m)%60);
+            /* set led output in CLOCK_SHOW mode*/
             buf_out->led = 128;
             break;
         case CLOCK_MODI:
+            /* set unsaved time to output in CLOCK_MODI mode*/
             sprintf(buf_out->fnd,"%02d%02d",clock_info->unsaved_h,clock_info->unsaved_m);
-            if(tm->tm_sec%2 == 0)buf_out->led = 16;
-            else buf_out->led = 32;
+            /* set led output blink in CLOCK_MODI mode */
+            if(tm->tm_sec%2 == 0)buf_out->led = 16; // When system sec is even, set led 4 on
+            else buf_out->led = 32;                 // When system sec is odd, set led 3 on
             break;                
     }
-
-    //save formatting string for time to buffer.
-    
-    /* dot_mat & text_led set empty*/
-    memset(buf_out->dot_matrix,0x00,MAX_DOT_MATRIX);
-    memset(buf_out->text_lcd, 0, MAX_TEXT_LCD);
 
     return;
 }
 
 void init_clock_info(struct clock_data *clock_info){
+    /* Initialize clock data */
     clock_info->clock_h =0;
 	clock_info->clock_m =0;
 	clock_info->clock_mode = 0;

6f9ec0a4b32136a255b562163fb0c90d268af240
gomamon <leeye9779@gmail.com>
2020-05-24 17:22:17 +0900

    comment

--- hw2/app/app.c
+++ hw2/app/app.c
@@ -7,8 +7,10 @@
 #include <fcntl.h>
 #include <asm/ioctl.h>
 
+/* dev_driver major number */
 #define MAJOR_NUM 242
 
+/* ioctl number */
 #define IOCTL_SET_OPTION _IOW(MAJOR_NUM, 0, char *)
 #define IOCTL_COMMAND _IO(MAJOR_NUM, 1)
 
@@ -22,48 +24,56 @@ int main(int argc, char **argv)
 	char c;
 	char params[12] ;
 
+	/* charck count of argument  */
 	if(argc != 4) { 
 		perror("Usage : TIMER_INTERVAL[1-100] TIMER_CNT[1-100] TIMER_INIT[0001-8000]\n");
 		return -1;
 	}
 
+	/* dev_driver device file open and check error*/
 	fd = open("/dev/dev_driver", O_RDWR);
 	if (fd<0){
 		perror("ERROR : Open Failured!\n");
 		return -1;
 	}
 
+	/* check argument for interval */
 	interval = atoi(argv[1]);
 	if(!(1<=interval && interval <=100)){
 		perror("TIMER_INTERVAL[1-100] \n");
 		return -1;
 	}
 
+	/* check argument for count */
 	cnt = atoi(argv[2]);
 	if(!(1<=interval && interval <=100)){
 		perror("TIMER_CNT[1-100]\n");
 		return -1;
 	}
 
+	/* check argument for init */
 	init = atoi(argv[3]);
 	if(!(1<=interval && interval <=8000)){
 		perror("TIMER_INIT[0001-8000]\n");
 		return -1;
 	}
 
+	/* make arguments of interval, count, init to string*/
 	sprintf(params, "%03d%03d%04d", interval, cnt, init);
-	printf("PARAMETER in USER : %s",params);
 
+	/* pass to argument to kernel using ioctl(IOCTL_SET_OPTION) */
 	c = ioctl(fd, IOCTL_SET_OPTION, params);
 	if(c < 0){
 		perror("IOCTL_SET_OPTION failed!");
 	}
 
+	/* start count using ioctl(IOCTL_COMMAND) */
 	c = ioctl(fd, IOCTL_COMMAND);
 	if(c < 0){
 		perror("IOCTL_COMMAND failed!");
 	}
 
+	/* close device file */
 	close(fd);
 
 	return 0;
--- hw2/module/dev_driver.c
+++ hw2/module/dev_driver.c
@@ -6,50 +6,49 @@
 #include <linux/ioctl.h>
 #include <asm/ioctl.h>
 #include <asm/io.h>
-//#include "device_driver.h"
 #include "fpga_dot_font.h"
 
+/* device drvier information */
 #define DEVICE_DRIVER_NAME "dev_driver"
 #define MAJOR_NUM 242
-#define BUF_LEN 50
 
-#define IOCTL_SET_OPTION _IOW(MAJOR_NUM, 0, char *)
-#define IOCTL_COMMAND _IO(MAJOR_NUM, 1)
-
+/* fpga physical address */
 #define PHY_LED 0x08000016
 #define PHY_FND 0x08000004
 #define PHY_DOT 0x08000210
 #define PHY_TEXT_LCD 0x08000090
 
+/* ioctl number */
+#define IOCTL_SET_OPTION _IOW(MAJOR_NUM, 0, char *)
+#define IOCTL_COMMAND _IO(MAJOR_NUM, 1)
 
-#define COMMAND_SIZE 12
+/* ioctl set_option parameter size */
+#define PARAM_SIZE 12
 
+/* device drivaer usage counter */
 static int dev_driver_usage = 0;
 
-long number = 0;
-int major_number;
-char *numchar;
-
-int timer_flag = 0;
-int count = 0;
-
+/* device driver function */
 int dev_driver_open(struct inode *,struct file *);
 int dev_driver_release(struct inode *, struct file *);
 long dev_driver_ioctl(struct file *, unsigned int, unsigned long);
 
+/* timer fuction */
 void timer_setup(void);
 void timer_handler(unsigned long);
 
+/* handling fpga function */
 void iomap_fpga(void);
 void iounmap_fpga(void);
 void fpga_clear(void);
 
 
+int count = 0;
+
 unsigned char text1[17] = "20161622";
 unsigned char text2[17] = "YEEUN LEE";
-int text1_pos = 0, text2_pos = 0;
-int text1_dir = 0, text2_dir = 0;
 
+/* file operations related to dev_driver module */
 static struct file_operations fops = {
 	.open = dev_driver_open,
 	.release = dev_driver_release,
@@ -57,24 +56,28 @@ static struct file_operations fops = {
 };
 
 static struct timer_data{
-	int interval;
-	int init;
-	int init_pos;
-	int count;	
-	struct timer_list timer;
+	int interval;	//timer interval
+	int init;		//init pattern
+	int init_pos;	//init pattern position
+	int count;		//timer count
+	int text1_pos;	//position in a first-line text_lcd
+	int text2_pos;	//position in a second-line text_lcd
+	int text1_dir;	//text moving direction in a first-line text_lcd
+	int text2_dir;	//text moving direction in a second-line text_lcd
+	struct timer_list timer;	//timer list struct
 } mydata;
 
+/* save kernal virtual address returned by ioremap() */
 static struct fpga_virtual_address{
-	unsigned char *led;
+	unsigned char *led;	
 	unsigned char *fnd;
 	unsigned char *dot;
 	unsigned char *text_lcd;
 } fpga_addr;
 
-// struct timer_data mydata;
-
 
 void iomap_fpga(){
+	/*save kernel virtual address mapped with the physical address space of fpga*/
 	fpga_addr.led = ioremap(PHY_LED, 0x1);
 	fpga_addr.fnd = ioremap(PHY_FND, 0x4);
 	fpga_addr.dot = ioremap(PHY_DOT,0x10);
@@ -82,6 +85,7 @@ void iomap_fpga(){
 }
 
 void iounmap_fpga(){
+	/*free the mapped address space by iomap */
 	iounmap(fpga_addr.led);
 	iounmap(fpga_addr.fnd);
 	iounmap(fpga_addr.dot);
@@ -90,22 +94,29 @@ void iounmap_fpga(){
 
 
 long dev_driver_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param){
+	/*device driver ioctl*/
+
 	int i;
 	char *temp;
 	int tmp;
 	char buff[12] = {'\0',};
-	printk("device_driver_ioctl\n");
 
 	switch(ioctl_num){
 		case IOCTL_SET_OPTION:
+			/* Take the parameters and store it in struct mydata for counter*/
+			printk("dev_driver ioctl set_option\n");
 			temp = (char *)ioctl_param;
 			
+			/*init struct mydata */
 			mydata.interval = 0;
 			mydata.count = 0;
 			count =0;
 			mydata.init = 0;
+			mydata.text1_pos = 0;	mydata.text2_pos = 0;
+			mydata.text1_dir = 0;	mydata.text2_dir = 0;
 
-			for(i=0; i<COMMAND_SIZE; i++){
+			/*read ioctl parameters(interval,count,init) and save to mydata*/
+			for(i=0; i<PARAM_SIZE; i++){
 				get_user(buff[i], temp+i);
 				if(buff[i]<'0'||buff[i]>'9') break;
 				tmp = (int)buff[i]-'0';
@@ -124,17 +135,18 @@ long dev_driver_ioctl(struct file *file, unsigned int ioctl_num, unsigned long i
 					}
 				}
 			}
-			text1_pos = 0;	text2_pos = 0;
-			text1_dir = 0;	text2_dir = 0;
+
 			break;
 		case IOCTL_COMMAND:
-			printk("IOCTL COMMAND");
-			init_timer(&(mydata.timer));
-			timer_setup();
-			printk("TIMER ON\n");
+			/* ioctl command
+			* set timer and start count  	
+			*/
+			printk("dev_driver ioctl command\n");
+			init_timer(&(mydata.timer)); //initailize timer
+			timer_setup();	//setup timer parameter and start count
 			break;
 		default:
-			printk("IOCTL ERROR\n");
+			printk("ERROR : invalid ioctl_num \n");
 			return -1;
 			break;
 	}
@@ -142,19 +154,21 @@ long dev_driver_ioctl(struct file *file, unsigned int ioctl_num, unsigned long i
 }
 
 void timer_setup(){
+	/*setup timer parameter and add timer*/
 
-	del_timer_sync(&mydata.timer);
+	del_timer_sync(&mydata.timer);	//delete before timer
 	
+	/*set timer inforamtion*/
 	mydata.timer.expires = jiffies + (mydata.interval *HZ);
 	mydata.timer.data = (unsigned long)&mydata;
 	mydata.timer.function = timer_handler;
 
-	add_timer(&mydata.timer);
-
+	add_timer(&mydata.timer);	//add timer
 	return;
 }
 
 int fpga_handler(struct timer_data *t_data){
+	/* write processed data in t_data to fpga */
 	int i;
 	
 	unsigned short led_value = (unsigned short) (1<<(8-t_data->init));
@@ -165,14 +179,13 @@ int fpga_handler(struct timer_data *t_data){
 	unsigned short int dot_value; 
 
 	unsigned short int text_lcd_value = 0;
-	unsigned char text_lcd_data[33] = {' ', };
+	unsigned char text_lcd_data[33];
 
 	/* led write*/
 	outw(led_value, (unsigned int)fpga_addr.led);
 
 	/* fnd write */
 	fnd_str[t_data->init_pos] = t_data->init;
-	printk("fnd: %d %d %d %d",fnd_str[0],fnd_str[1],fnd_str[2],fnd_str[3]);
 	fnd_value = fnd_str[0]<<12 | fnd_str[1]<<8 | fnd_str[2]<<4 | fnd_str[3];
 	outw(fnd_value, (unsigned int)fpga_addr.fnd);
 
@@ -186,10 +199,10 @@ int fpga_handler(struct timer_data *t_data){
 	/* text lcd write*/
 	for(i=0; i<32; i++)	text_lcd_data[i] = ' ';
 	for(i=0; i< 8; i++){
-		text_lcd_data[i+text1_pos]=text1[i];
-		text_lcd_data[16+(i+text2_pos)] = text2[i];
+		text_lcd_data[i+t_data->text1_pos]=text1[i];
+		text_lcd_data[16+(i+t_data->text2_pos)] = text2[i];
 	}
-	text_lcd_data[16+(8+text2_pos)]=text2[8];
+	text_lcd_data[16+(8+t_data->text2_pos)]=text2[8];
 
 	for(i=0;i<32;i++)
     {
@@ -202,6 +215,7 @@ int fpga_handler(struct timer_data *t_data){
 }
 
 void fpga_clear(){
+	/* write clear in fpga */
 	int i;
 	unsigned char c = ' ';
 	unsigned short int text_lcd_value = 0;
@@ -220,79 +234,98 @@ void fpga_clear(){
 	/* text lcd clear*/
 	for(i=0;i<32;i++)
     {
-        // text_lcd_value = ((c & 0xFF) << 8) | (c & 0xFF);
 		text_lcd_value = (c & 0xFF) << 8 | (c & 0xFF) ;
 		outw(text_lcd_value,(unsigned int)fpga_addr.text_lcd+i);
         i++;
     }
 
+	return;
+
 }
 
 void timer_handler(unsigned long timeout){
+	/* timer callback function to handle timer 
+	* Write processed timer data to fpga and increase count and process other timer datas
+	* If timer count is over count(set by ioctl set_options), clear fpga and return.
+	* If not, processing timer data and add timer
+	*/
 	struct timer_data *t_data = (struct timer_data*)timeout;
-	printk("timer handler %d\n", t_data->count);
+	printk("timer count : %d\n", t_data->count);
 
-	if(fpga_handler(t_data) == -1)
+	if(fpga_handler(t_data) != 0)
 		return;
 
-	if(text1_dir == 0) text1_pos++;
-	else if(text1_dir == 1) text1_pos--;
-	if(text1_pos+8 >= 16){
-		text1_dir = 1;
-	}
-	else if(text1_pos <= 0 ){
-		text1_dir = 0;
-	}
-
-	if(text2_dir == 0) text2_pos++;
-	else if(text2_dir == 1) text2_pos--;
-	if(text2_pos+9 >= 16){
-		text2_dir = 1;
-	}
-	else if(text2_pos <= 0 ){
-		text2_dir = 0;
-	}
-
+	/* increase count */
 	t_data->count++;
-	t_data->init++;
 	
+	/* set init pattern and init pattern position*/
+	t_data->init++;
 	if(t_data->init > 8) t_data->init = 1;
 	
 	if((t_data->count)%8 == 0)
 		t_data->init_pos = 1 + t_data->init_pos;
 	if(t_data->init_pos == 4) t_data->init_pos = 0;
 
+	/* set text position for text lcd*/
+	if(t_data->text1_dir == 0) t_data->text1_pos++;
+	else if(t_data->text1_dir == 1) t_data->text1_pos--;
+	if(t_data->text1_pos+8 >= 16){
+		t_data->text1_dir = 1;
+	}
+	else if(t_data->text1_pos <= 0 ){
+		t_data->text1_dir = 0;
+	}
+
+	if(t_data->text2_dir == 0) t_data->text2_pos++;
+	else if(t_data->text2_dir == 1) t_data->text2_pos--;
+	if(t_data->text2_pos+9 >= 16){
+		t_data->text2_dir = 1;
+	}
+	else if(t_data->text2_pos <= 0 ){
+		t_data->text2_dir = 0;
+	}
+
+	/* if this timer count over count(set by ioctl set_options) fpga clear and return*/
 	if(t_data -> count > count ){
 		fpga_clear();
 		return ;
 	}
-	
+
+	/* set timer information */
 	mydata.timer.expires = get_jiffies_64() + (mydata.interval * HZ);
 	mydata.timer.data = (unsigned long)&mydata;
 	mydata.timer.function = timer_handler;
 
+	/* add timer */
 	add_timer(&mydata.timer);
 
 	return;
 }
 
 int dev_driver_open(struct inode *inode, struct file *file) {
-	printk("device_driver_open\n");
-	if (dev_driver_usage != 0) {
+	/* dev_driver open */
+	printk("dev_driver open\n");
+
+	if (dev_driver_usage != 0) { //if usage counter set, return -EBUSY 
 		return -EBUSY;
 	}
-	dev_driver_usage = 1;
+
+	dev_driver_usage = 1; //set usage counter
+
 	return 0;
 }
 
 int dev_driver_release(struct inode *inode, struct file *file) {
-	printk("device_driver_release\n");
-	dev_driver_usage = 0;
+	/* dev_driver release */
+	printk("dev_driver release\n");
+	dev_driver_usage = 0;	//unset usage counter
 	return 0;
 }
 
 
 int __init dev_driver_init(void){
+	/* initialize module */
+	int major_number;
 	printk("device_driver_init\n");
 
 	major_number = register_chrdev(MAJOR_NUM, DEVICE_DRIVER_NAME, &fops);
@@ -300,24 +333,23 @@ int __init dev_driver_init(void){
 		printk( "error %d\n", major_number);
 		return major_number;
 	}
-	
-	major_number = MAJOR_NUM;
-	iomap_fpga();
-	printk("dev_file: /dev/%s , major: %d\n", DEVICE_DRIVER_NAME, major_number);
 
+	iomap_fpga(); // iomap fpga devices
+	printk("dev_file: /dev/%s , major: %d\n", DEVICE_DRIVER_NAME, MAJOR_NUM);
 	printk("init module\n");
 	return 0;
 }
 
 void __exit dev_driver_exit(void){
-
+	/* exit module */
 	printk("device_driver_exit\n");
-	dev_driver_usage = 0;
-	del_timer_sync(&mydata.timer);
-	iounmap_fpga();
+	dev_driver_usage = 0;	//unset usage counter
+	del_timer_sync(&mydata.timer);	//delete timer
+	iounmap_fpga();	//iounmap fpga address
 	unregister_chrdev(MAJOR_NUM, DEVICE_DRIVER_NAME);
 }
 
 module_init( dev_driver_init );
 module_exit( dev_driver_exit );
-MODULE_LICENSE("Dual BSD/GPL");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("YEEUN");
